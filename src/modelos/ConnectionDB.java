ed3b39281654e09494849ecd1f780e432f5a1bf5
sebastianPabon <pabonlopezsebastian@gmail.com>
2022-06-23 20:19:01 -0500

    Inicializacion proyecto
    * Se crea el formulario de inicio de sesion

---
+++ src/modelos/ConnectionDB.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author sebas
+ */
+public class ConnectionDB {
+    public Connection cadena_conexion(){
+        //Permite almacenar la conección
+        Connection cn = null;
+
+        //Obtener los errores si llega a fallar la conexión
+        try{
+            //obtener el driver de JDBC de sql server
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            
+            //Realizar el string de conexión a la base de datos
+            cn = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-C386PL9;databaseName=escuela;user=sa;password=12345;encrypt=true;trustServerCertificate=true");
+
+            System.out.println("Se conecto Correctamente");
+        }catch (Exception e){
+            //Si se genera algun error lo capturamos y mostramos
+            System.out.println(e);
+        }
+        return cn;
+    }
+}
